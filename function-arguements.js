1fdd71d4dae9d538a014c2e8392dd68e6e998cbc
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-02-15 01:50:48 +0000

    javascripting workshop pt2

---
+++ function-arguements.js
@@ -0,0 +1,4 @@
+function math (ref1, ref2, ref3){
+  return (ref2 * ref3) + ref1
+}
+console.log(math(53, 61, 67))
---
+++ object-keys.js
@@ -0,0 +1,10 @@
+const car = {
+  make: 'Honda',
+  model: 'Accord',
+  year: 2020
+}
+
+// const keys = Objects.keys(car)
+// console.log(keys)
+
+console.log(Object.keys(car))
